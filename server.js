13bfa5cb32197c39291f9095f8bccbccba837129
Jason <jason@ultraculture.org>
2022-04-10 15:26:27 -0500

    Server running

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port = 4001;
 
 const { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
@@ -8,3 +9,10 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
